7d31f333305711de5928c16351f7134ec9c60745
Aidan Froggatt <aidanfr@live.ca>
2024-09-30 15:57:32 -0400

    added basic google colors, noto sans font, and support for system dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     extend: {
       fontFamily: {
         sans: [
-          '"Inter"',
+          '"Noto Sans"',
           "ui-sans-serif",
           "system-ui",
           "sans-serif",
@@ -16,7 +16,27 @@ export default {
           '"Noto Color Emoji"',
         ],
       },
+      colors: {
+        google: {
+          blue: "#174EA6",
+          red: "#A50E0E",
+          orange: "#E37400",
+          green: "#0D652D",
+          mediumBlue: "#4285F4",
+          mediumRed: "#EA4335",
+          yellow: "#FBBC04",
+          mediumGreen: "#34A853",
+          lightBlue: "#D2E3FC",
+          lightRed: "#FAD2CF",
+          lightYellow: "#FEEFC3",
+          lightGreen: "#CEEAD6",
+          lightGrey: "#F1F3F4",
+          grey: "#9AA0A6",
+          black: "#202124",
+        },
+      },
     },
   },
+  darkMode: 'media', // Apply dark mode based on user's system preference
   plugins: [],
 } satisfies Config;
