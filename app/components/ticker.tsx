81381670bd8a69ce1e8ffadcc811a3be6a034506
Aidan Froggatt <aidanfr@live.ca>
2024-10-06 20:08:44 -0400

    ticker images responsive

--- app/components/ticker.tsx
+++ app/components/ticker.tsx
@@ -7,18 +7,22 @@ interface TickerProps {
 const Ticker: React.FC<TickerProps> = ({ items }) => {
   return (
     <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
-      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 animate-infinite-scroll">
         {items.map((item) => (
           <li key={item.id}>
-            <Image src={item.src} alt={item.alt} height={100} width={100} />
+            <div className="relative w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32">
+              <Image src={item.src} alt={item.alt} layout="fill" objectFit="contain" />
+            </div>
           </li>
         ))}
       </ul>
       {/* Duplicate the same list for the infinite scroll effect */}
-      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
+      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 animate-infinite-scroll" aria-hidden="true">
         {items.map((item) => (
           <li key={`${item.id}-duplicate`}>
-            <Image src={item.src} alt={item.alt} height={100} width={100} />
+            <div className="relative w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32">
+              <Image src={item.src} alt={item.alt} layout="fill" objectFit="contain" />
+            </div>
           </li>
         ))}
       </ul>
