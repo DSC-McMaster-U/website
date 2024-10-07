acc5f50e9994090f0d9ccc74f3f0a0b67362d3e5
Aidan Froggatt <aidanfr@live.ca>
2024-10-06 22:58:21 -0400

    navlinks now handle single links, group links, and both

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,28 +7,41 @@ import { FiCalendar, FiChevronDown, FiStar } from "react-icons/fi";
 // Updated interface to include icon and description
 interface NavLinkProps {
     name: string;
-    links: { href: string; label: string; description: string; icon: JSX.Element }[]; // Add icon and description here
+    links?: { href: string; label: string; description: string; icon: JSX.Element }[]; // Add icon and description here
+    href?: string;
 }
 
-const NavLink = ({ name, links }: NavLinkProps) => {
+const NavLink = ({ name, links, href }: NavLinkProps) => {
     return (
         <li className="relative group">
-            <span className="flex items-center cursor-pointer transition-colors duration-200 gap-x-1 dark:text-google-grey dark:group-hover:text-white dark:group-hover:bg-google-grey px-3 py-1 rounded-full">
-                {name}
-                {links.length > 0 && (
-                    <FiChevronDown className="w-4 h-4 transition-transform duration-200 transform group-hover:rotate-180" />
-                )}
-            </span>
-            {links.length > 0 && (
+            {href ? (
+                <Link href={href} className="flex items-center cursor-pointer transition-colors duration-200 gap-x-1 dark:text-google-grey dark:group-hover:text-white dark:group-hover:bg-google-grey px-3 py-1 rounded-full">
+                    {name}
+                    {links && (
+                        <FiChevronDown className="w-4 h-4 ml-1 transition-transform duration-200 transform group-hover:rotate-180" />
+                    )}
+                </Link>
+            ) : (
+                <span className="flex items-center cursor-pointer transition-colors duration-200 gap-x-1 dark:text-google-grey dark:group-hover:text-white dark:group-hover:bg-google-grey px-3 py-1 rounded-full">
+                    {name}
+                    {links && (
+                        <FiChevronDown className="w-4 h-4 ml-1 transition-transform duration-200 transform group-hover:rotate-180" />
+                    )}
+                </span>
+            )}
+            {links && (
                 <div className="absolute left-0 hidden group-hover:block p-4 dark:bg-google-black rounded-lg border dark:border-google-grey">
                     <ul className="flex flex-col gap-y-4">
                         {links.map((link, index) => (
                             <li key={index} className="flex items-center">
-                                <Link href={link.href} className="block whitespace-nowrap w-full dark:text-google-grey dark:hover:text-white transition-colors duration-200">
+                                <Link
+                                    href={link.href}
+                                    className="block whitespace-nowrap w-full dark:text-google-grey dark:hover:text-white transition-colors duration-200"
+                                >
                                     <div className="flex flex-row items-center gap-x-2">
-										{link.icon}
+                                        {link.icon}
                                         <div className="flex flex-col">
-                                            <span className="text-sm dark:text-white">{link.label}</span>
+                                            <span className="text-base dark:text-white">{link.label}</span>
                                             <span className="text-xs">{link.description}</span>
                                         </div>
                                     </div>
@@ -63,23 +76,25 @@ const Header = () => {
         },
         { 
             name: "Newsletters", 
-            links: [] 
+            href: "/newsletters",
         },
     ];
 
     return (
         <header className="flex flex-row justify-between fixed z-50 bg-google-black w-full p-4">
             <div className="flex flex-row items-center h-full gap-x-8">
-                <Image
-                    src={Icon}
-                    alt={"Icon"}
-                    width={50}
-                    height={50}
-                />
-                <nav className="lg:flex hidden flex-row">
+                <Link href="/">
+                    <Image
+                        src={Icon}
+                        alt={"Icon"}
+                        width={50}
+                        height={50}
+                    />
+                </Link>
+                <nav className="lg:flex hidden flex-row text-base">
                     <ul className="flex flex-row">
                         {navLinks.map((navLink, index) => (
-                            <NavLink key={index} name={navLink.name} links={navLink.links} />
+                            <NavLink key={index} name={navLink.name} links={navLink.links} href={navLink.href} />
                         ))}
                     </ul>
                 </nav>
