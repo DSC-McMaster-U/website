b3a87b77139909124e2858ca467e17ec4b5fc584
Aidan Froggatt <aidanfr@live.ca>
2024-10-07 17:43:25 -0400

    separated dropdown component

---
+++ app/components/dropdown.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import { useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
+
+interface DropdownProps {
+    name: string;
+    children: React.ReactNode;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ name, children }) => {
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const handleToggle = () => {
+        setDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleMouseEnter = () => {
+        setDropdownOpen(true);
+    };
+
+    const handleMouseLeave = () => {
+        setDropdownOpen(false);
+    };
+
+    return (
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <button
+                onClick={handleToggle}
+                className="flex items-center gap-x-1 px-3 py-1 rounded-full hover:text-google-grey cursor-pointer transition-colors duration-200"
+            >
+                {name}
+                <FiChevronDown className={`w-4 h-4 transition-transform duration-200 ${isDropdownOpen ? "rotate-180" : ""}`} />
+            </button>
+
+            {isDropdownOpen && (
+                <div className="shadow absolute left-0 p-4 rounded-lg bg-white dark:bg-google-black">
+                    {children}
+                </div>
+            )}
+        </div>
+    );
+};
+
+interface DropdownItemProps {
+    href: string;
+    label: string;
+    icon?: JSX.Element;
+    description?: string;
+}
+
+export const DropdownLink: React.FC<DropdownItemProps> = ({ href, label, icon, description }) => {
+    return (
+        <Link href={href} className="block whitespace-nowrap w-full text-google-grey hover:text-google-black dark:hover:text-white transition-colors duration-200">
+            <div className="flex flex-row items-center gap-x-2">
+                {icon && <>{icon}</>}
+                <div className="flex flex-col">
+                    <span className="text-base text-google-black dark:text-white">{label}</span>
+                    {description && <span className="text-xs">{description}</span>}
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default Dropdown;
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,81 +1,103 @@
+"use client"; // Ensures client-side behavior for this component
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Icon from "@/app/icon.svg";
 import { SiDiscord, SiInstagram, SiLinkedin } from "react-icons/si";
-import { FiCalendar, FiChevronDown, FiStar } from "react-icons/fi";
+import { FiCalendar, FiMenu, FiStar, FiX } from "react-icons/fi";
+import Dropdown, { DropdownLink } from "./dropdown";
+
+
+const Accordian = () => {
+	
+	return (
+		<Link href={""}>
+
+		</Link>
+	)
+};
 
 interface NavLinkProps {
     name: string;
-    links?: { href: string; label: string; description: string; icon: JSX.Element }[];
     href?: string;
+    links?: { href: string; label: string; description?: string; icon?: JSX.Element }[];
 }
 
-const NavLink = ({ name, links, href }: NavLinkProps) => {
+const NavLink: React.FC<NavLinkProps> = ({ name, href, links }) => {
     return (
-        <li className="relative cursor-default group text-google-black dark:text-white group-hover:text-google-grey">
-            {href ? (
-                <Link href={href} className="flex items-center transition-colors duration-200 gap-x-1 px-3 py-1 rounded-full group-hover:text-google-grey">
-                    {name}
-                    {links && (
-                        <FiChevronDown className="w-4 h-4 transition-transform duration-200 transform group-hover:rotate-180" />
-                    )}
-                </Link>
-            ) : (
-                <span className="flex items-center transition-colors duration-200 gap-x-1 px-3 py-1 rounded-full group-hover:text-google-grey">
-                    {name}
-                    {links && (
-                        <FiChevronDown className="w-4 h-4 transition-transform duration-200 transform group-hover:rotate-180" />
-                    )}
-                </span>
-            )}
-            {links && (
-                <div id="dropdown" className="shadow absolute left-0 hidden group-open:block group-hover:block p-4 rounded-lg">
-                    <ul className="flex flex-col gap-y-4">
+        <li className="relative cursor-default group text-google-black dark:text-white">
+            {links ? (
+                <Dropdown name={name}>
+					<ul className="flex flex-col gap-y-4">
                         {links.map((link, index) => (
-                            <li key={index} className="flex items-center">
-                                <Link
-                                    href={link.href}
-                                    className="block whitespace-nowrap w-full text-google-grey hover:text-google-black dark:hover:text-white transition-colors duration-200"
-                                >
-                                    <div className="flex flex-row items-center gap-x-2">
-                                        {link.icon}
-                                        <div className="flex flex-col">
-                                            <span className="text-base text-google-black dark:text-white">{link.label}</span>
-                                            <span className="text-xs">{link.description}</span>
-                                        </div>
-                                    </div>
-                                </Link>
+                            <li key={index}>
+                                <DropdownLink href={link.href} label={link.label} icon={link.icon} description={link.description} />
                             </li>
                         ))}
                     </ul>
-                </div>
+				</Dropdown>
+            ) : (
+				<span className="flex items-center transition-colors duration-200 gap-x-1 px-3 py-1 rounded-full hover:text-google-grey cursor-pointer">
+                    { href &&
+						<Link href={href} className="flex items-center gap-x-1">
+                        	{name}
+						</Link>
+					}
+                </span>
             )}
         </li>
     );
 };
 
 const Header = () => {
+    const [isPopupOpen, setPopupOpen] = useState(false);
+
+    // Disables scrolling behind the popup when it's open
+    useEffect(() => {
+        if (isPopupOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+    }, [isPopupOpen]);
+
     const navLinks = [
-        { 
-            name: "Events", 
+        {
+            name: "Events",
             links: [
-                { 
-                    href: "/events", 
-                    label: "All Events", 
-                    description: "Workshops, socials, and more", 
-                    icon: <FiCalendar />
+                {
+                    href: "/events",
+                    label: "All Events",
+                    description: "Workshops, socials, and more",
+                    icon: <FiCalendar />,
                 },
-                { 
-                    href: "https://solutionchallenge.gdscmcmasteru.ca/", 
-                    label: "Solution Challenge", 
-                    description: "Test your skills", 
-                    icon: <FiStar />
-                }
-            ] 
+                {
+                    href: "https://solutionchallenge.gdscmcmasteru.ca/",
+                    label: "Solution Challenge",
+                    description: "Test your skills",
+                    icon: <FiStar />,
+                },
+            ],
         },
-        { 
-            name: "Newsletters", 
+        {
+            name: "Newsletters",
             href: "/newsletters",
+        },
+    ];
+
+    const socialMedia = [
+        {
+            href: "https://www.instagram.com/gdscmcmasteru/",
+            icon: <SiInstagram className="w-full h-full" />,
+        },
+        {
+            href: "https://discord.gg/gdscmcmasteru",
+            icon: <SiDiscord className="w-full h-full" />,
+        },
+        {
+            href: "https://www.linkedin.com/company/gdscmcmasteru/",
+            icon: <SiLinkedin className="w-full h-full" />,
         },
     ];
 
@@ -83,12 +105,7 @@ const Header = () => {
         <header className="flex flex-row justify-between fixed z-50 bg-white dark:bg-google-black w-full p-4">
             <div className="flex flex-row items-center h-full gap-x-8">
                 <Link href="/">
-                    <Image
-                        src={Icon}
-                        alt={"Icon"}
-                        width={50}
-                        height={50}
-                    />
+                    <Image src={Icon} alt="Icon" className="h-6 w-auto" />
                 </Link>
                 <nav className="lg:flex hidden flex-row text-base">
                     <ul className="flex flex-row">
@@ -99,13 +116,43 @@ const Header = () => {
                 </nav>
             </div>
             <div className="hidden lg:flex flex-row items-center space-x-4">
-                <SiDiscord className="cursor-pointer text-google-black hover:text-google-grey dark:text-white transition-colors duration-200" />
-                <SiInstagram className="cursor-pointer text-google-black hover:text-google-grey dark:text-white transition-colors duration-200" />
-                <SiLinkedin className="cursor-pointer text-google-black hover:text-google-grey dark:text-white transition-colors duration-200" />
+                {socialMedia.map((social, index) => (
+                    <Link key={index} href={social.href} target="_blank" className="flex justify-center items-center w-6 h-6 cursor-pointer text-google-black hover:text-google-grey dark:text-white transition-colors duration-200" >
+                        {social.icon}
+                    </Link>
+                ))}
             </div>
-            <nav className="flex lg:hidden flex-row">
-                {/* hamburger menu navigation */}
-            </nav>
+
+            {/* Full-screen popup toggle */}
+            <button className="lg:hidden" onClick={() => setPopupOpen(true)}>
+                <FiMenu className="w-6 h-6" />
+            </button>
+
+            {/* Popup content */}
+			{isPopupOpen && (
+				<div
+					className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50"
+				>
+					<div
+						className="bg-white dark:bg-google-black p-8 rounded-lg w-full min-h-screen max-h-screen overflow-auto"
+					>
+						<div className="flex justify-end">
+							<button onClick={() => setPopupOpen(false)}>
+								<FiX className="w-6 h-6 text-google-black dark:text-white" />
+							</button>
+						</div>
+						<div>
+							<nav>
+								<ul className="flex flex-col gap-y-4">
+									{navLinks.map((navLink, index) => (
+										<NavLink key={index} name={navLink.name} links={navLink.links} href={navLink.href} />
+									))}
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			)}
         </header>
     );
 };
