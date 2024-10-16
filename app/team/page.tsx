33577920cb63fc8f9305d83c2ecb1f35bf8f0b42
johnsh9656 <harrisonjohns1219@gmail.com>
2024-10-15 20:54:07 -0400

    created team page

---
+++ app/team/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// will be async once fetching data
+const TeamPage = () => {
+  // await fetch team data
+
+  return (
+    <div>TeamPage</div>
+
+  )
+}
+
+export default TeamPage
