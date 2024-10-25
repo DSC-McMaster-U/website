4af44be1ea6b692737255256a9db64f96dc6dc5a
Aidan Froggatt <aidanfr@live.ca>
2024-10-24 21:59:25 -0400

    created cards for newsletters

--- app/newsletters/page.tsx
+++ app/newsletters/page.tsx
@@ -2,6 +2,8 @@ import { client } from "@/sanity/lib/client";
 import { Newsletter } from "@/types/sanity";
 import { Metadata } from "next";
 import Link from "next/link";
+import { ChevronArrowSpan } from "../components/ChevronArrow";
+import Tag from "../components/Tag";
 
 export const metadata: Metadata = {
     title: "Newsletters | GDSC McMaster U",
@@ -12,7 +14,7 @@ const fetchNewsletters = async () => {
   const newsletters = await client.fetch(
     `*[_type == 'newsletter']{
             title,
-            subtitle,
+            description,
             slug,
             body,
             _updatedAt
@@ -26,22 +28,24 @@ const NewslettersPage = async () => {
 
   return (
     <main>
-      <section id="newsletters" className="flex md:flex-row flex-col gap-y-8 md:gap-y-0">
+      <section id="newsletters" className="flex flex-col gap-y-4">
         <h2>Newsletters</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <p>Through GDSC McMaster University&apos;s monthly newsletter, stay updated on the latest tech news, events, and innovations. Featuring industry trends, club highlights, and upcoming activities, the newsletter connects members to valuable insights and opportunities in the tech world.</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-10">
           {newsletters.map((newsletter) => (
-            <div
+            <Link 
+              href={`/newsletters/${newsletter.slug.current}`} 
               key={newsletter.slug.current}
-              className="border rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-200"
+              className="button-arrow group relative group w-full bg-white dark:bg-google-grey p-1 dark:bg-opacity-10 rounded-md overflow-hidden shadow-lg border dark:border-google-black border-b-google-lightGrey transition-all duration-200 hover:shadow-2xl"
             >
-              <Link href={`/newsletters/${newsletter.slug.current}`}>
-                <h2 className="text-lg font-bold">{newsletter.title}</h2>
-                <p className="text-sm text-gray-600">{newsletter.description}</p>
-                <p className="text-xs text-gray-400">
-                  {new Date(newsletter._updatedAt).toLocaleDateString()}
-                </p>
-              </Link>
-            </div>
+                <div className="bg-google-black p-4">
+                  <h5>{newsletter.title}</h5>
+                </div>
+                <div className="p-4 gap-y-4 flex flex-col">
+                  <p>{newsletter.description}</p>
+                  <ChevronArrowSpan>Read now</ChevronArrowSpan>
+                </div>
+            </Link>
           ))}
         </div>
       </section>
