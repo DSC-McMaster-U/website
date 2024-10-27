7c6e444f28638c6c5045daec6136e65681dce0c0
coolguy-stack <nafieu.alif@gmail.com>
2024-10-26 21:36:23 -0400

    added header and footer to the Events page and refined the event cards similar to the newsletter page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,6 +2,9 @@ import { client } from "@/sanity/lib/client";
 import { Event } from "@/types/sanity";
 import { Metadata } from "next";
 import Link from "next/link";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+import LinkTitleCard from "@/app/components/LinkTitleCard";
 
 export const metadata: Metadata = {
     title: "Events | GDSC McMaster U",
@@ -25,25 +28,27 @@ const EventsPage = async () => {
   const Events: Event[] = await fetchEvents();
 
   return (
-    <div>
-      <h1>Events</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {Events.map((Events) => (
-          <div
-            key={Events.slug.current}
-            className="border rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-200"
-          >
-            <Link href={`/events/${Events.slug.current}`}>
-              <h2 className="text-lg font-bold">{Events.title}</h2>
-              <p className="text-sm text-gray-600">{Events.description}</p>
-              <p className="text-xs text-gray-400">
-                {new Date(Events._updatedAt).toLocaleDateString()}
-              </p>
-            </Link>
+    <>
+      <Header />
+      <main>
+        <section id="Events" className="flex flex-col gap-y-4">
+          <h2>Events</h2>
+          <p>Through GDSC McMaster University&apos;s events, stay updated on the latest tech news, events, and innovations. Featuring industry trends, club highlights, and upcoming activities, the events connect members to valuable insights and opportunities in the tech world.</p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-10">
+            {Events.map((Events) => (
+              <LinkTitleCard
+                key={Events.slug.current}
+                title={Events.title}
+                link={`/events/${Events.slug.current}`}
+              >
+                <p>{Events.description}</p>
+              </LinkTitleCard>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 };
 
