bad0602386fd526e30a260a1238c73b7d03bec3c
Aidan Froggatt <aidanfr@live.ca>
2025-01-28 10:45:47 -0500

    lint fixes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ import SectionCard from "./components/SectionCard";
 import Card from "./components/Card";
 import Heart from "./components/svgs/Heart";
 import AnimatedHero, { AnimatedHeroSvg } from "./components/AnimatedHero";
-import SocialMediaAnimation from "./components/SocialMediaAnimation";
 
 const HeroSection = async () => {
   const generalInfo: GeneralInfo = await client.fetch(`*[_type == 'generalInfo'][0]`);
