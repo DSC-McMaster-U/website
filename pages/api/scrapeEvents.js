96913c702b49f3e194adf1bc7e1ba09553459157
JosephMaltese <josephmaltese34@gmail.com>
2025-02-03 20:15:25 -0500

    Fetched from gdg api instead

--- pages/api/scrapeEvents.js
+++ pages/api/scrapeEvents.js
@@ -3,20 +3,27 @@ import * as cheerio from 'cheerio';
 // Api route to scrape event data from the GDG event page to display on our site.
 export default async function handler (req, res) {
     const url = "https://gdg.community.dev/gdg-on-campus-mcmaster-university-hamilton-canada/";
+    const url2 = "https://gdg.community.dev/api/event_slim/for_chapter/2428";
     // Ensure the request method is GET
     if (req.method === "GET") {
         try {
             // Fetch the html from the event page
-            const response = await fetch(url);
+            const response = await fetch(url2);
             // Convert response to a string
-            const data = await response.text();
+            //const data = await response.text();
+            const data = await response.json();
+            const results = data.results;
+            console.log("Results:", results);
 
             // Arrays to store event data
             const upcomingEvents = []
             const pastEvents = []
 
             // Load the html into cheerio
-            const $ = cheerio.load(data);
+            // const $ = cheerio.load(data);
+
+            // USE THIS NOW, RETURNS A JSON
+            // https://gdg.community.dev/api/event_slim/for_chapter/2428
 
             // ITERATES THROUGH EVERY <a> but doesnt get correct output
             // $("[data-testid='data-block-for-pastEvents-mu9W6Ekd-k3']>a").each((index, element) => { 
@@ -60,47 +67,7 @@ export default async function handler (req, res) {
 
 
 
-            const pastEventsInfo = $("[data-testid='data-block-for-pastEvents-mu9W6Ekd-k3'] > a > div:last" );
-
-            pastEventsInfo.each((index, element) => {
-                console.log("Element:", $(element).html());
-                console.log('\n\n\n');
-            })
-            // pastEventsInfo
-            // $("[data-testid='data-block-for-pastEvents-mu9W6Ekd-k3'] > a[data-testid='container-block-24RneQKmFRW']").each((index, element) => {
-            //     console.log("Element:", $(element).html());
-            //     const link = $(element).attr("href");
-            //     console.log("Link:", link);
-
-            //     // Extract event title
-            //     const title = $(element).find("a.link-styles__link_1ec3q span").last().text().trim();
-            //     console.log("Title:", title);
-
-            //     // Extract date
-            //     const dateText = $(element).find("span.plainText-styles__plainText_8ys50").first().text().trim();
-            //     const formattedDate = dateText.replace(/[^\w\s,]/g, ''); // Remove special characters
-            //     console.log("Date:", formattedDate);
-
-            //     // Extract event type
-            //     const eventType = $(element).find("span.plainText-styles__plainText_8ys50").eq(1).text().trim();
-            //     console.log("Event Type:", eventType);
-
-            //     if (title && link) {
-            //         pastEvents.push({ title, link, formattedDate, eventType });
-            //     }
-            // });
-
-
-
-
-
-
-
-
-
-
-
-            res.status(200).json({ $ });
+            res.status(200).json({ "Message" : "Data Scraped Successfully" });
 
 
         } catch (error) {
