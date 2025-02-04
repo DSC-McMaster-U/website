6819a056ea75b9493c791e19519c98802b805888
JosephMaltese <josephmaltese34@gmail.com>
2025-02-03 22:39:20 -0500

    Finished scraping from gdg site, started events page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,10 +1,15 @@
 "use client";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from "@/app/components/Header";
+import Image from 'next/image'
 
 
 const eventsPage = () => {
-    const scrapeEvents = async () => {
+    const [pastEvents, setPastEvents] = useState([]);
+    const [upcomingEvents, setUpcomingEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getEvents = async () => {
         try {
             const res = await fetch('/api/scrapeEvents', {
                 method: 'GET',
@@ -12,7 +17,11 @@ const eventsPage = () => {
                     'Content-Type': 'application/json',
                 },
             });
-            console.log("Success:", res);
+            const data = await res.json();
+            setPastEvents(data["past_events"]);
+            setUpcomingEvents(data["upcoming_events"]);
+            setLoading(false);
+            console.log(data["past_events"]);
 
         } catch (error) {
             console.error(error);
@@ -20,13 +29,50 @@ const eventsPage = () => {
     }
 
     useEffect(() =>{
-        scrapeEvents();
+        getEvents();
     }, []);
+
   return (
     <>
         <Header />
         <main className="mt-20">
-            <h1>Events Page</h1>
+            <div className="mx-auto">
+                <h1>Events</h1>
+            </div>
+            <div className="mx-auto">
+                <h2>Upcoming Events</h2>
+                <ul>
+                    {upcomingEvents ? upcomingEvents.map((upcomingEvent, index) => {
+                        return (
+                            <li key={index}>hi</li>
+                        );
+
+                    }) : (loading ? <p>Loading...</p> : <p>No upcoming events</p>)}
+                </ul>
+                
+
+            </div>
+            <div className="mx-auto">
+                <h2>Past Events</h2>
+                <ul>
+                    {pastEvents ? pastEvents.map((pastEvent, index) => {
+                        return (
+                            <li key={index}>
+                                <Image src={pastEvent.picture}
+                                    width={200}
+                                    height={200}
+                                    alt="Event Image"
+                                    className="w-auto h-auto"
+                                />
+                                <p>{pastEvent.start_date}</p>
+                                <a><p>{pastEvent.title}</p></a>
+                                <p>{pastEvent.chapter_title}</p>
+                            </li>
+                        );
+                    }) : (loading ? <p>Loading...</p> : <p>No past events</p>)}
+
+                </ul>
+            </div>
         </main>
       
     </>
--- pages/api/scrapeEvents.js
+++ pages/api/scrapeEvents.js
@@ -1,73 +1,23 @@
-import * as cheerio from 'cheerio';
-
 // Api route to scrape event data from the GDG event page to display on our site.
 export default async function handler (req, res) {
-    const url = "https://gdg.community.dev/gdg-on-campus-mcmaster-university-hamilton-canada/";
-    const url2 = "https://gdg.community.dev/api/event_slim/for_chapter/2428";
+    const pastEventsUrl = "https://gdg.community.dev/api/event_slim/for_chapter/2428/?status=Completed";
+    const upcomingEventsUrl = "https://gdg.community.dev/api/event_slim/for_chapter/2428/?status=Live";
     // Ensure the request method is GET
     if (req.method === "GET") {
         try {
-            // Fetch the html from the event page
-            const response = await fetch(url2);
-            // Convert response to a string
-            //const data = await response.text();
-            const data = await response.json();
-            const results = data.results;
-            console.log("Results:", results);
+            // Fetch the past events data from the GDG event page
+            const response1 = await fetch(pastEventsUrl).then((res) => res.json());
+            const pastEvents = response1.results;
+            console.log("Past Events Results:", pastEvents);
 
-            // Arrays to store event data
-            const upcomingEvents = []
-            const pastEvents = []
-
-            // Load the html into cheerio
-            // const $ = cheerio.load(data);
-
-            // USE THIS NOW, RETURNS A JSON
-            // https://gdg.community.dev/api/event_slim/for_chapter/2428
-
-            // ITERATES THROUGH EVERY <a> but doesnt get correct output
-            // $("[data-testid='data-block-for-pastEvents-mu9W6Ekd-k3']>a").each((index, element) => { 
-            //     //console.log(`Event HTML at index ${index}:`, $(element).html());
-                
-                
-            //     const dateText = $(element).find('span.plainText-styles__plainText_8ys50').first().text().trim();
-            //     const formattedDate = dateText.replace(/[^\w\s,]/g, '');
-            //     console.log("Date:", formattedDate);
-
-            //     const eventType = $(element).find('span.plainText-styles__plainText_8ys50').eq(1).text().trim();
-            //     console.log("Event Type:", eventType);
-
-            //     const title = $(element).find('span.plainText-styles__plainText_8ys50').eq(2).text().trim();
-            //     console.log("Title:", title);
-                
-            //     const link = $(element).attr("href");
-            //     console.log("Link:", link);
-            //     console.log('iteration ', index);
-            // })
+            // Fetch the upcoming events data from the GDG event page
+            const response2 = await fetch(upcomingEventsUrl).then((res) => res.json());
+            const upcomingEvents = response2.results;
+            console.log("Upcoming Events Results:", upcomingEvents);
 
 
-
-            // GETS CORRECT OUTPUT BUT DOESNT ITERATE THROUGH EVERY <a>
-            // $("[data-testid='data-block-for-pastEvents-mu9W6Ekd-k3']").each((index, element) => {
-            //     const dateText = $(element).find('span .plainText-styles__plainText_8ys50').first().text().trim();
-            //     const formattedDate = dateText.replace(/[^\w\s,]/g, '');
-            //     console.log("Date:", formattedDate);
-
-            //     const eventType = $(element).find('span .plainText-styles__plainText_8ys50').eq(1).text().trim();
-            //     console.log("Event Type:", eventType);
-
-            //     const title = $(element).find('span .plainText-styles__plainText_8ys50').eq(2).text().trim();
-            //     console.log("Title:", title);
-                
-            //     const link = $(element).find("a").attr("href");
-            //     console.log("Link:", link);
-                
-            //     console.log("index", index);
-            // })
-
-
-
-            res.status(200).json({ "Message" : "Data Scraped Successfully" });
+            // Return the past and upcoming events data
+            res.status(200).json({ "past_events": pastEvents, "upcoming_events": upcomingEvents });
 
 
         } catch (error) {
