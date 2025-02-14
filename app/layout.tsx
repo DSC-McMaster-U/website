72a129ca5c08c0ce0d91063c94bcb1c3eb52c205
Aidan Froggatt <aidanfr@live.ca>
2025-02-14 17:12:04 -0500

    SEO fixes, icon updates, added manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Google Developer Group on Campus | McMaster University",
   description: "Google Developer Group on Campus at McMaster University bridges the gap between theory and practice through solving real-world problems.",
-  icons: [
-    {
-      url: "/icon.svg",
-    },
-  ],
 };
 
 export default function RootLayout({
