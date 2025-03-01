23e36841fe5156bc7b249fd83abe4e1fab227cf0
JosephMaltese <josephmaltese34@gmail.com>
2025-03-01 02:15:49 -0500

    Sorted events by date

--- app/lib/getEvents.tsx
+++ app/lib/getEvents.tsx
@@ -1,3 +1,11 @@
+interface Event {
+    start_date: string,
+    event_type: string,
+    title: string,
+    description_short: string,
+    picture: string,
+  }
+
 async function getEvents () {
     const pastEventsUrl = "https://gdg.community.dev/api/event_slim/for_chapter/2428/?status=Completed";
     const upcomingEventsUrl = "https://gdg.community.dev/api/event_slim/for_chapter/2428/?status=Live";
@@ -18,6 +26,10 @@ async function getEvents () {
         const pastEvents = pastData.results;
         const upcomingEvents = upcomingData.results;
 
+        // Sorts the past events by date in descending order according to timestamp (most recent events first).
+        pastEvents.sort((a: Event, b: Event) => new Date(b.start_date).getTime() - new Date(a.start_date).getTime());
+        // Upcoming events are sorted in ascending order so that the soonest upcoming event is displayed first.
+        upcomingEvents.sort((a: Event, b: Event) => new Date(a.start_date).getTime() - new Date(b.start_date).getTime());
 
         // Return the past and upcoming events data
         return { "past_events": pastEvents, "upcoming_events": upcomingEvents};
