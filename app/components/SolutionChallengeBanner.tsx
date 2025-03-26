b035d2f05680536b044b87d0053025d9faaaaf11
johnsh9656 <harrisonjohns1219@gmail.com>
2025-03-26 13:46:59 -0400

    update hover effects on solution challenge banner

--- app/components/SolutionChallengeBanner.tsx
+++ app/components/SolutionChallengeBanner.tsx
@@ -6,11 +6,11 @@ const SolutionChallengeBanner = () => {
   const solutionChallengeLink = "https://gdg.community.dev/events/details/google-gdg-on-campus-mcmaster-university-hamilton-canada-presents-gdg-on-campus-mcmaster-solutions-challenge-2025-info/";
 
   return (
-    <div className="bg-blue-500 text-white py-8 px-8 rounded-lg shadow-lg mt-6">
+    <div className="bg-blue-500 text-white py-6 px-8 rounded-lg shadow-lg transition-all duration-500 hover:scale-[101%]">
       <div className="flex flex-row justify-between md:justify-center md:gap-24 items-center">
         <h1 className="text-4xl font-bold">🚀 Join the new Solution Challenge!</h1>
         <Link href={solutionChallengeLink} className='h-fit'>
-          <ChevronArrowButton className="bg-red-500 text-white-00 border-2 dark:border-black-00 border-white-00 min-w-44 items-center">
+          <ChevronArrowButton className="bg-red-500 hover:bg-red-600 transition-all duration-300 text-white-00 border-2 dark:border-black-00 border-white-00 min-w-44 items-center">
             <span className="font-semibold p-1 px-2 text-lg">Learn More</span>
           </ChevronArrowButton>
         </Link>
